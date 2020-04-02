3192f9ffcfd4a97ae6553b89ddd82700df39c5b7
Ragaey M.Ragaa <ragaeymuhammed@gmail.com>
2020-04-02 06:12:37 +0200

    initial commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def load_url_from_file(file_path: str):
+    #TODO: add the code neededto read the text file with the url in it
+    pass
+
+def load_page(url: str):
+    #TODO: add the code that reads the url
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: analyze the text
+    pass
